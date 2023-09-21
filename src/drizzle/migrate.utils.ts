38cb12b0da1c8ae18a4649f42134ec0f3b608904
zachuri <punsalang.zachary@gmail.com>
2023-09-21 01:50:52 -0700

    database/ -> drizzle/: updated file names and updated package.json scripts

--- src/drizzle/migrate.utils.ts
+++ src/drizzle/migrate.utils.ts
@@ -1,13 +1,13 @@
-import {migrate} from 'drizzle-orm/node-postgres/migrator';
-import db from '@/database/database.config';
+import { migrate } from 'drizzle-orm/node-postgres/migrator';
+import db from '@/drizzle/drizzle.config';
 
 // this will automatically run needed migrations on the database
-migrate(db, {migrationsFolder: './src/database/migrations'})
+migrate(db, { migrationsFolder: './src/drizzle/migrations' })
   .then(() => {
     console.log('Migrations complete!');
     process.exit(0);
   })
-  .catch((err) => {
+  .catch(err => {
     console.error('Migrations failed!', err);
     process.exit(1);
   });
