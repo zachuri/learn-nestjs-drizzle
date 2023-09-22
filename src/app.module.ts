4676d0ebd6d14dd8c4df597e8bf96d919f0ddc07
ZACHURI <punsalang.zachary@gmail.com>
2023-09-22 12:02:07 -0700

    app.module: added format fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
-import {Global, Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
-import {UserModule} from './user/user.module';
-import {ConfigModule} from '@nestjs/config';
-import {GlobalModule} from './global/global.module';
+import { Global, Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UserModule } from "./user/user.module";
+import { ConfigModule } from "@nestjs/config";
+import { GlobalModule } from "./global/global.module";
 
 @Global()
 @Module({
-  imports: [UserModule, ConfigModule.forRoot({isGlobal: true}), GlobalModule],
+  imports: [UserModule, ConfigModule.forRoot({ isGlobal: true }), GlobalModule],
   controllers: [AppController],
   providers: [AppService],
 })
